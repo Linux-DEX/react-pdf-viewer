37f911013baf6da9e8fc3ad2e693be5c893dc9d9
Linux-DEX <sarabjeet.9353@gmail.com>
2024-08-08 09:00:20 +0530

    highlight click and pdf click working

--- src/PdfViewer.js
+++ src/PdfViewer.js
@@ -17,22 +17,22 @@ const PdfViewer = ({pdfFile}) => {
       height: 1.5,
       id: "joseph",
     },
-    {
-      pageIndex: 0,
-      top: 31.7,
-      left: 54.7,
-      width: 9.9,
-      height: 1.5,
-      id: "hypertension",
-    },
-    {
-      pageIndex: 0,
-      top: 31.7,
-      left: 65.3,
-      width: 6.4,
-      height: 1.5,
-      id: "disbetes",
-    },
+    //{
+    //  pageIndex: 0,
+    //  top: 31.7,
+    //  left: 54.7,
+    //  width: 9.9,
+    //  height: 1.5,
+    //  id: "hypertension",
+    //},
+    //{
+    //  pageIndex: 0,
+    //  top: 31.7,
+    //  left: 65.3,
+    //  width: 6.4,
+    //  height: 1.5,
+    //  id: "disbetes",
+    //},
   ]);
 
   useEffect(() => {
@@ -97,10 +97,60 @@ const PdfViewer = ({pdfFile}) => {
     trigger: Trigger.None,
   });
 
+  useEffect(() => {
+    const pdfViewer = viewerRef.current;
+
+    if (pdfViewer) {
+      const pdfViewerInstance = pdfViewer.querySelector('.rpv-core__viewer');
+      
+      if (pdfViewerInstance) {
+        const handleLinkClick = (event) => {
+          const target = event.target;
+
+          if (target.tagName === 'A' && target.href) {
+            event.preventDefault();
+            //console.log('Link clicked:', target.href);
+            alert("link is working")
+
+            const annotationElement = target.closest('.rpv-core__annotation');
+
+            if (annotationElement) {
+              const { style } = annotationElement;
+              console.log('Annotation style properties:');
+              console.log('Height:', style.height);
+              console.log('Left:', style.left);
+              console.log('Top:', style.top);
+              console.log('Width:', style.width);
+            }
+          }
+        };
+
+        if(pdfViewerInstance){
+          pdfViewerInstance.addEventListener('click', handleLinkClick);
+          console.log(pdfViewerInstance)
+        }
+
+        return () => {
+          pdfViewerInstance.removeEventListener('click', handleLinkClick);
+        };
+      }
+    }
+  }, [viewerRef]);
+
   return (
     <div>
+      {selectedText && (
+        <div>
+          <h4>Selected Text:</h4>
+          <p>{selectedText}</p>
+          <h4>Bounding Box:</h4>
+          <p>{`X: ${boundingBox.x}, Y: ${boundingBox.y}, Width: ${boundingBox.width}, Height: ${boundingBox.height}`}</p>
+        </div>
+      )}
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer fileUrl={pdfFile} plugins={[highlightPluginInstance]} />
+        <div style={{ height: '88vh' }} ref={viewerRef}>
+          <Viewer fileUrl={pdfFile} plugins={[highlightPluginInstance]} />
+        </div>
       </Worker>
     </div>
   );
